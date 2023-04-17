9230bc2431f3b4a7aab23197daf4828e623895b4
momenabdulrazek <momen.abdelrazek33@gmail.com>
2023-04-17 13:43:09 +0000

    Intial awaken bringup

---
+++ BoardConfig.mk
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2017-2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+TARGET_KERNEL_VERSION := 4.9
+
+# Inherit from common msm8953-common
+include device/xiaomi/msm8953-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/tissot
+
+# Display
+TARGET_SCREEN_DENSITY := 440
+
+# Filesystem
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+BOARD_USES_RECOVERY_AS_BOOT := true
+TARGET_NO_RECOVERY := true
+
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+
+# Kernel
+TARGET_KERNEL_CONFIG := tissot_defconfig
+
+# Partitions
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 55087422464 # 25765059584 - 16384
+
+# Camera
+TARGET_SUPPORT_HAL1 := false
+
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
+
+# RIL
+ENABLE_VENDOR_RIL_SERVICE := true
+
+# Security Patch Level
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
+# Treble
+BOARD_VENDORIMAGE_PARTITION_SIZE := 629145600
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
+PRODUCT_FULL_TREBLE_OVERRIDE := true
+# Inherit the proprietary files
+include vendor/xiaomi/tissot/BoardConfigVendor.mk
